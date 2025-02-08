53ce0a177fd6d8236011c35513733d311eefd8f2
abdulhadi-selise <abdulhadi@selise.ch>
2025-02-08 13:36:29 +0600

    added router

---
+++ src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateTrip = () => {
+    return (
+        <div>
+            <h2>Create Trip</h2>
+        </div>
+    );
+};
+
+export default CreateTrip;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { RouterProvider, createBrowserRouter } from "react-router";
+import CreateTrip from "./create-trip";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+      path: "/create-trip",
+      element: <CreateTrip />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
